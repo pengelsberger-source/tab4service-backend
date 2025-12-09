2f3acb983ead4a41b9c9df7ad78cf3b269874f1a
pengelsberger-source <p.engelsberger@icloud.com>
2025-12-09 22:44:44 +0100

    Update send.js

--- api/send.js
+++ api/send.js
@@ -6,13 +6,13 @@ export default async function handler(req, res) {
 
   const message = `📣 *Service Anfrage*\n\n🪑 *Tisch:* ${table}\n🔧 *Aktion:* ${type}\n⏰ *Zeit:* ${new Date().toLocaleTimeString()}`;
 
-  const telegramURL = `https://api.telegram.org/bot${8353027423:AAFjTbVtKPXzajNt13KpVyOGdjrWBhW8YCA}/sendMessage`;
+  const telegramURL = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
   await fetch(telegramURL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      chat_id: 7225728131,
+      chat_id: chatId,
       text: message,
       parse_mode: "Markdown"
     })
